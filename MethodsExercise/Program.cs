135018d7102866e0dd567d65501aa2cc49cc2e37
Blaine <blaineprickett@gmail.com>
2022-10-18 21:54:16 -0500

    add sum

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,9 +1,23 @@
 ﻿namespace MethodsExercise
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+        public static int Sum(int num1, int num2)
         {
+            var answer = num1 + num2;
+            return answer;
+        }
+
+        private static void Main(string[] args)
+        {
+            if (args is null)
+            {
+                throw new ArgumentNullException(nameof(args));
+            }
+
+            _ = Sum(2, 6);
+
+
             // Q & A - Exercise 1
 
             Console.WriteLine("Hello, what is your name?");
@@ -20,7 +34,7 @@
                 var band = Console.ReadLine();
             Console.WriteLine("Here is a fun little story for you.");
 
-            // Fun Story - Exercise 2
+            // Fun Story 
 
             Console.WriteLine($"There once was a person named {name} who lived in a {color} house that was three stories tall. {name} had two roomates. " +
                 $"One was a kangaroo and the other was a {animal}. They enjoyed spending time together going to movies and concerts. {name} really enjoyed" +
@@ -28,6 +42,10 @@
                 $" and opened the pit all the way up. Everyone was moshing and going wild. They all had a blast, but by the end {name} was all bruised up. It was totally worth it, he said. " +
                 $" So they began their long walk home to their three story {color} castle.");
             Console.ReadLine();
+
+            // Exercise 2
+
+
 
 
         }
